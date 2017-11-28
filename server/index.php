e5153a8d882fd493af24a9e75453c28236ed207b
ofirsSwiego <ofir@swiego.com>
2017-11-28 15:17:43 +0200

    first commit

---
+++ server/index.php
@@ -0,0 +1,88 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+date_default_timezone_set("Asia/Jerusalem");
+$ERROR_NUMBER = '';
+// Check connection
+require 'Connected.php';
+$link = ConnectToDb::connect();
+mysqli_set_charset( $link, 'utf8');
+
+
+$json = '';
+$json['result'] = false;
+
+if($_POST['action'] == 'sendData') {
+    $data = json_decode($_POST['data']);
+    var_dump($data);
+    if (!$data->email)
+        return ['error' => 'not email'];
+
+
+    $date = new \DateTime();
+    $sql = "INSERT INTO lead_users (name,email,phone,date) VALUES
+                ('" . $data->name . "',
+                '" . $data->email . "',
+                '" . $data->phone . "',
+                 '" . date_format($date, 'd-m-Y H:i:s') . "')";
+
+
+    if (mysqli_query($link, $sql)) {
+        $ERROR_NUMBER = 22;
+        $json['result']['status'] = $ERROR_NUMBER;
+        echo json_encode($json);
+
+        //echo "Records added successfully.";
+        $sos = '';
+
+        $txt = '';
+        $txt .= '<div>';
+        $txt .= "Name: " . $data->name . "<br>";
+        $txt .= "Email:" . $data->email . "<br>";
+        $txt .= "Phone:  " . $data->phone . "<br>";
+        $txt .= "</div>";
+        //$to = "ofirs1988@gmail.com";
+        $subject = "Swiego SITE new lead";
+        $headers = "From: office@swiego.com" . "\r\n" .
+            "CC: office@swiego.com";
+        require 'PHPMailer/PHPMailerAutoload.php';
+        $mail = new PHPMailer;
+
+        try {
+            $mail->SMTPDebug = 2;                              // Enable verbose debug output
+
+            $mail->isSMTP();                                    // Set mailer to use SMTP
+            $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
+            $mail->Port = 587;                                    // TCP port to connect to
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'office@swiego.com';                 // SMTP username
+            $mail->Password = 'swiego55555';                           // SMTP password
+            $mail->Charset = 'UTF-8';
+            $mail->AddAddress('office@swiego.com', 'Swiego One');
+            $mail->AddAddress('ofir@swiego.com', 'Ofir shurdeker');
+            //$mail->AddAddress('evgeni@swiego.com', 'evgeni fomenko');
+            //$mail->AddAddress('gal@swiego.com', 'evgeni fomenko');
+            $mail->setFrom('office@swiego.com', 'Swiego');
+            //$mail->addAddress($to);     // Add a recipient
+
+            $mail->isHTML(true);                                  // Set email format to HTML
+
+            $mail->Subject = '=?utf-8?B?' . base64_encode($subject) . '?=';
+            $mail->Body = $txt;
+            $mail->AltBody = $txt;
+            $last_id = $link->insert_id;
+            if (!$mail->send()) {
+                echo 'Message could not be sent.';
+                echo 'Mailer Error: ' . $mail->ErrorInfo;
+            } else {
+                echo 'Message has been sent';
+            }
+            $errors[] = "Send mail sucsessfully";
+        } catch (phpmailerException $e) {
+            $errors[] = $e->errorMessage(); //Pretty error messages from PHPMailer
+        } catch (Exception $e) {
+            $errors[] = $e->getMessage(); //Boring error messages from anything else!
+        }
+    }
+}
+
